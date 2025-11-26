bbf1911c259b14fb8728bd66697d47bfe04f791a
Max <maksme99@gmail.com>
2025-11-26 14:05:46 +0300

    fix

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const OpenAI = require("openai");
+require("dotenv").config();
 const client = new OpenAI({
   apiKey: process.env.API_KEY,
 });
